6489b6fc12c5f9074d42b67b26a72abcc15f2812
limsehun <tpgns8714852@gmail.com>
2024-09-05 17:13:53 +0900

    타임리프, 마이바티스

--- demoProject3/src/main/java/edu/kh/demo/mapper/TestMapper.java
+++ demoProject3/src/main/java/edu/kh/demo/mapper/TestMapper.java
@@ -2,7 +2,21 @@ package edu.kh.demo.mapper;
 
 import org.apache.ibatis.annotations.Mapper;
 
+/* @Mapper
+ * - 마이바티스 mapper와 연결된 읹터페이스임을 명시
+ * - 자동으로 해당 인터페이스를 상속 받은 클래스를 만들어 Bean으로 등록
+ */
 @Mapper
 public interface TestMapper {
+	
+	/**
+	 * 
+	 * @param userNo
+	 * @return userName
+	 */
+		String selectUserName(int userNo);
+		// -> 해당 메서드가 호출된 경우 연결되어져 있는 mapper.xml파일에서 id속성값이
+		//    메서드명과 같은 sql 태그가 수행된다!
+
 
 }
